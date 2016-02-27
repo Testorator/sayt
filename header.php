c1c1c27783a7d4c77a550091201ca969a3c38ec3
Rainos <mazik0496@mail.ru>
2016-02-27 20:19:19 +1000

    create for github

---
+++ header.php
@@ -0,0 +1,26 @@
+<div id="logo">
+    Logotype
+</div>
+<div id="site_header">
+    <a class="menu_button" href="header.php">
+        О нас
+    </a>
+    <a class="menu_button">
+        Магазин
+        <ul class="hiden_list">
+            <li class="hiden_list_item">Первый пункт</li>
+            <li class="hiden_list_item">Второй пункт</li>
+            <li class="hiden_list_item">Третий пункт</li>
+            <li class="hiden_list_item">Четвертый пункт</li>
+        </ul>
+    </a>
+    <a class="menu_button">
+        Условия доставки
+    </a>
+    <a class="menu_button">
+        Отзывы
+    </a>
+    <a class="menu_button">
+        Контакты
+    </a>
+</div>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+    <link rel="stylesheet" type="text/css" href="general_style.css">
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    <link rel="stylesheet" type="text/css" href="car_specifications_page.css">  
+    <title>Эксперементальная версия сайта</title>
+</head>
+<body id="page">
+    <?php 
+    require('header.php');
+    require('car_specifications_page.php');
+    require('footer.php');
+    ?>
+</body>
+</html>
